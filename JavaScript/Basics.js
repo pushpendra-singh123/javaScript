3df10cec91d2fd22b46f1d8ac87e03c42bc9728b
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2024-07-18 20:42:04 +0530

    starting backend dev by Sanket Singh

---
+++ JavaScript/Basics.js
@@ -0,0 +1,34 @@
+// console.log(`this is back-tick string`);
+
+// console.log(undefined); // show sense of emptyness
+
+// //object : stores key value pair
+// console.log({ key: "raghav" });
+
+// // Symbol : immutable
+// console.log(Symbol("Hello"))
+
+// console.log(typeof(154)) // return o/p as String
+// console.log(typeof(-0))
+// console.log(typeof(undefined))
+// console.log(typeof(null))
+// console.log(typeof(NaN))
+
+// process.stdout.write("Hello ,");
+// process.stdout.write(" i'm raghav");
+
+// console.log(typeof("5"+ 5));
+
+// console.log(3>1 && "");
+// console.log(3>1 && "Raghav");
+
+// loops
+let i = 1;
+while (i < 10) {
+  console.log(i);
+  i += 1;
+}
+
+for (let i = 10; i < 21; i++) {
+  console.log(i);
+}
